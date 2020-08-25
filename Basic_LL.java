c708a40cc10da28c03f7ea61a538b129a7229e4f
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-08-25 15:16:51 +0530

    Create Basic_LL.java

---
+++ Basic_LL.java
@@ -0,0 +1,13 @@
+public class Basic_LL
+   {
+      public static void main(String[] args) 
+	     {
+	          LinkedList<String> pavan=new LinkedList<>();      //Creating Linkedlist
+	          pavan.add("Abd");                                 
+	          pavan.add("vk");
+            pavan.add("kl");
+	          System.out.println(pavan);                     
+ 
+	    }
+ }
+                
