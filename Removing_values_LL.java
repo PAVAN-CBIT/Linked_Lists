5df421ee66650169923ca39820f108718cad4a98
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-08-25 17:13:21 +0530

    Create Removing_values_LL.java

---
+++ Removing_values_LL.java
@@ -0,0 +1,25 @@
+public  class Removing_values_LL
+{
+      public static void main(String[] args)
+      {
+
+            LinkedList<String> LL = new LinkedList<>();  //Creating LinkedList with String
+
+
+                                                         //Adding elements to the LinkedList
+            LL.add("a");
+            LL.add("b");
+            LL.add("c");
+            System.out.println(LL);                      //Printing the values before removing
+            LL.remove("a");
+            LL.remove(1);
+            System.out.println(LL);                      //Printing the values after removing
+
+
+
+
+
+
+      }
+
+}
