bd5f77f4b27e81646389ecbcc5ae7452ce5475ea
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-08-25 17:23:08 +0530

    Create Adding_First_Last_Values_LL.java

---
+++ Adding_First_Last_Values_LL.java
@@ -0,0 +1,19 @@
+public  class Adding_First_Last_values_LL
+{
+  public static void main(String[] args)
+  {
+
+      LinkedList<String> LL = new LinkedList<>();  //Creating LinkedList with String
+      
+      
+                                                 //Adding elements to the LinkedList
+      LL.add("a");
+      LL.addLast("b");
+      LL.addFirst("c");
+      System.out.println(LL);                      //Printing the values
+
+
+
+  }
+  
+}
