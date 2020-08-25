933a1f74810d1451b916a8db9fce45d53def24b4
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-08-25 17:07:44 +0530

    Create Adding_values_LL_.java

---
+++ Adding_values_LL_.java
@@ -0,0 +1,19 @@
+public  class Adding_values_LL
+{
+  public static void main(String[] args)
+  {
+
+      LinkedList<String> LL = new LinkedList<>();  //Creating LinkedList with String
+      
+      
+                                                 //Adding elements to the LinkedList
+      LL.add("a");
+      LL.add("b");
+      LL.add("c");
+      System.out.println(LL);                      //Printing the values
+
+
+
+  }
+  
+}
