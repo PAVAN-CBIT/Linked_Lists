99b8f470e39676ab5abc052383d8705243c18df3
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-08-25 15:26:30 +0530

    Update Basic_LL.java

--- Basic_LL.java
+++ Basic_LL.java
@@ -1,13 +1,16 @@
 public class Basic_LL
-   {
-      public static void main(String[] args) 
-	     {
-	          LinkedList<String> pavan=new LinkedList<>();      //Creating Linkedlist
-	          pavan.add("Abd");                                 
-	          pavan.add("vk");
-            pavan.add("kl");
-	          System.out.println(pavan);                     
- 
-	    }
- }
-                
+{
+	public static void main(String[] args) 
+	{
+		LinkedList<String> pavan = new LinkedList<>();      // Defining Linkedlist with String elements
+
+
+		                                                    // Adding String values to the Linked 
+		pavan.add("Abd");                                 
+		pavan.add("vk");
+		pavan.add("kl");
+		System.out.println(pavan);                          // Printing the values                  
+
+	}
+}
+
