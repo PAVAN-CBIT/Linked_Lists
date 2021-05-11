d41da0950de61dd9478ba8e7717d02578c3fc28a
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-05-11 11:25:28 +0530

    Create Linkedlist_Numbers_Print.java

---
+++ Linkedlist_Numbers_Print.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class Main
+
+{
+
+	  public static void main(String[] args) 
+	   {
+		   LinkedList<Integer> words=new LinkedList<Integer>();
+		   words.add(10);
+		   words.add(15);
+		   words.add(20);
+		
+		  for(int word:words)
+		    {
+		    
+		      System.out.print(words)  
+		   }
+	}
