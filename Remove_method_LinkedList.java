50111ab5c900b570d7949368a1b3d51db6bd9271
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-05-21 08:40:18 +0530

    Create Remove_method_LinkedList.java

---
+++ Remove_method_LinkedList.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+public class Main
+{
+
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+		LinkedList<Integer>Number=new LinkedList<Integer>();
+		Number.add(10);
+		Number.add(15);
+		Number.add(20);
+		Number.add(25);
+		Number.remove();
+		for(int Numbers:Number)
+		{
+		    System.out.println(Numbers);
+		}
+	}
+}
